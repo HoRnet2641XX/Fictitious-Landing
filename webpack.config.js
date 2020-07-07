ac76aa8738746fe21262c0918e222665760119ac
Takamichi.Nasu <>
2020-07-08 00:29:48 +0900

    ejs,file-loader設定追加

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devServer: {
     contentBase: 'dist',
     open: true,
-    port: 9002,
+    port: 9101,
   },
   /// ファイルの出力設定/
   output: {
@@ -24,10 +24,11 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: [
           'style-loader',
-          'css-loader',
+          'css-loader?url=false',
           {
             loader: 'postcss-loader',
             options: {
+              url: true,
               plugins: [
                 require('autoprefixer')({
                   browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4'],
@@ -41,18 +42,6 @@ module.exports = {
         ],
       },
       {
-        test: /.(js|jsx)$/,
-        include: [path.resolve(__dirname, 'src/js')],
-        loader: 'babel-loader',
-      },
-      // // ↓ 追加
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        include: [path.resolve(__dirname, 'src/js')],
-        loader: 'eslint-loader',
-      },
-      {
         test: /\.(jpg|png)$/,
         use: [
           {
@@ -63,16 +52,34 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /.(js|jsx)$/,
+        include: [path.resolve(__dirname, 'src/js')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'src/js')],
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.ejs$/,
+        use: ['html-loader', 'ejs-html-loader'],
+      },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html',
+      template: 'src/index.ejs',
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles/bundle_[name].css',
+      filename: 'styles/bundle.[name].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src', to: '' }],
     }),
   ],
 };
